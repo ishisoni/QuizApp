8084f10382712b26360339db0852f7056f11deb0
Simin Savani <38788938+simin4950@users.noreply.github.com>
2020-04-11 18:57:10 -0400

    Update FragmentSix.java

--- app/src/main/java/edu/uga/cs/quizapp/FragmentSix.java
+++ app/src/main/java/edu/uga/cs/quizapp/FragmentSix.java
@@ -29,53 +29,73 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+/**
+* FragmentOne class creates a Fragment for the first country question
+* using onCreateView method.
+*/
 public class FragmentSix extends Fragment {
+    // elements from layout 
     TextView question;
-
     RadioButton button1, button2, button3;
     Button moveButton;
-
     RadioGroup group;
+    
+    // ArrayLists and String variables for answer choices 
     ArrayList<String> wrongContinents = new ArrayList<String>();
     ArrayList<String> answerChoices = new ArrayList<String>();
-
     QuizDBHelper db;
     String correctAnswer = "";
 
+    /**
+    * FragmentOne is an empty public constructor
+    */
     public FragmentSix() {
-
         // Required empty public constructor
-
     }
 
+    /**
+    * onCreateView is a method that creates the view for the fragment.
+    * @param inflater, container, savedInstanceState
+    * @return View
+    */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
+        // Intialize the view with the layout
         View view = inflater.inflate(R.layout.fragment_six, container, false);
+        
+        // Find the question placeholder on layout
         question = (TextView) view.findViewById(R.id.section_label);
-
-
-
+        
+        // Find the RadioButtons on the layout
         button1 = (RadioButton) view.findViewById(R.id.radioButton1);
         button2 = (RadioButton) view.findViewById(R.id.radioButton2);
         button3 = (RadioButton) view.findViewById(R.id.radioButton3);
+        
+        // Find moveButton on the layout and start Final.class intent if selected 
         moveButton = (Button) view.findViewById(R.id.button5);
         Intent intent = new Intent(getActivity(), Final.class);
         moveButton.setOnClickListener(e -> {
             startActivity(intent);
         });
+        
+        // Generate 3 answer choices by have 2 incorrect and 1 correct continent 
+        // Add continents to wrongContinents
         wrongContinents.add("Asia");
         wrongContinents.add("Africa");
         wrongContinents.add("North America");
         wrongContinents.add("South America");
         wrongContinents.add("Europe");
         wrongContinents.add("Oceania");
+        
+        // Set countryName as the question
         String countryName = QuizDBHelper.countries.get(5);
         question.setText(countryName);
+        // Set countryName to correctAnswer and add it to answerChoices
         correctAnswer = MainActivity.hs.get(countryName);
         answerChoices.add(correctAnswer);
+        
+        // Get other random incorrect countries and add answerChoices, only 3 answers
         Random randomGenerator = new Random();
         while(answerChoices.size() <3) {
             int random = randomGenerator.nextInt(6);
@@ -84,11 +104,14 @@ public class FragmentSix extends Fragment {
             }
         }
         Log.d("ANSWER CHOICES", " " +answerChoices.toString());
+        
+        // Shuffle answerChoices and set the RadioButtons to the answerChoices
         Collections.shuffle(answerChoices);
         button1.setText(answerChoices.get(0));
         button2.setText(answerChoices.get(1));
         button3.setText(answerChoices.get(2));
 
+        // Check if button is selected and updated answers array if correct
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,6 +126,8 @@ public class FragmentSix extends Fragment {
                 }
             }
         });
+        
+        // Check if button is selected and updated answers array if correct
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,6 +143,8 @@ public class FragmentSix extends Fragment {
 
             }
         });
+        
+        // Check if button is selected and updated answers array if correct
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -133,18 +160,9 @@ public class FragmentSix extends Fragment {
 
             }
         });
-
+        
+        // Return view 
         return view;
 
     }
-
-
-
-
-
 }
-
-
-
-
-
